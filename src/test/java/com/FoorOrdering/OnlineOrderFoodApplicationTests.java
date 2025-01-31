a31dd75f02a2692edfc293e1a4ed53622164aea2
Lakeserl <dinhlam18112003@gmail.com>
2025-01-31 13:01:01 +0700

    commit

---
+++ src/test/java/com/FoorOrdering/OnlineOrderFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoorOrdering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrderFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
